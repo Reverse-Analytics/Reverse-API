fcd2d4fdee1f8b31c751cc66c92d834d72202c0b
Miraziz.Khidoyatov <khidoyatov1997@gmail.com>
2024-04-02 17:39:24 +0500

    Create base test classes

---
+++ Reverse-Analytics/Tests/ReverseAnalytics.Tests.Unit/Controllers/ControllerFixtureBase.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+
+namespace ReverseAnalytics.Tests.Unit.Controllers;
+
+public abstract class ControllerFixtureBase : FixtureBase
+{
+    protected readonly HeaderDictionary _headerDictionary;
+    protected readonly ControllerContext _controllerContext;
+
+    protected ControllerFixtureBase()
+    {
+        _headerDictionary = [];
+        _controllerContext = GetControllerContext();
+    }
+
+    private ControllerContext GetControllerContext()
+    {
+        var httpResponseMock = new Mock<HttpResponse>();
+        httpResponseMock.SetupGet(h => h.Headers).Returns(_headerDictionary); // Set Headers property to the new HeaderDictionary
+
+        var httpContextMock = new Mock<HttpContext>();
+        httpContextMock.SetupGet(c => c.Response).Returns(httpResponseMock.Object);
+
+        var controllerContext = new ControllerContext { HttpContext = httpContextMock.Object };
+
+        return controllerContext;
+    }
+}
